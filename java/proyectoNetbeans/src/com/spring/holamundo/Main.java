8a0181f83071af4182d18c30bd27791402531bac
armando.gomez <armando.gomez@jesus-dev.develop>
2013-01-08 15:48:41 -0600

    Se agrega el package en donde estarán mis ejemplos de Spring

---
+++ java/proyectoNetbeans/src/com/spring/holamundo/Main.java
@@ -0,0 +1,9 @@
+package com.spring.holamundo;
+
+//http://www.dosideas.com/cursos/course/view.php?id=13
+//http://www.javatutoriales.com/2010/12/contenedores-de-ioc-e-inyeccion-de.html
+public class Main {
+    public static void main(String[] args){
+        
+    }
+}
