762594022e16316a8221e560d1c93359d801850f
sabio <armandojpr@yahoo.com>
2014-04-04 20:57:47 -0600

    Agregue unos algoritmos de ordenamiento (burbuja y quicksort) hechos en python

---
+++ python/cosasDesorganizadas/algoritmosDeOrdenamiento/burbuja.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+
+class Burbuja(object):
+
+	def __init__(self, arreglo):
+		self.arreglo = arreglo
+
+	""" BURBUJA
+	def ordena(self):
+		iteracion = 1
+		n = len(self.arreglo)
+		
+		for i in range(1,n):
+			for j in range(0,n-1):
+				if self.arreglo[j] > self.arreglo[j+1] :
+					aux = self.arreglo[j]
+					self.arreglo[j] = self.arreglo[j+1] 
+					self.arreglo[j+1] = aux
+				print "iteracion %s  arreglo = %s" %( iteracion,self.arreglo )
+				iteracion = iteracion + 1
+	"""
+
+	"""BURBUJA MEJORADO """
+	def ordena(self):
+		iteracion = 1
+		n = len(self.arreglo)
+		i = 1
+		ordenado = False
+
+		while (i<n and ordenado == False):
+			i = i + 1
+			ordenado = True
+			for j in range(0, n-1):
+				if self.arreglo[j] > self.arreglo[j+1] :
+					ordenado = False
+					aux = self.arreglo[j]
+					self.arreglo[j] = self.arreglo[j+1] 
+					self.arreglo[j+1] = aux
+				print "iteracion %s  arreglo = %s" %( iteracion,self.arreglo )
+				iteracion = iteracion + 1
+
+b = Burbuja([2,7,3,93,1,4,33,50,5])
+b.ordena()
+print "Final = %s" %b.arreglo
---
+++ python/cosasDesorganizadas/algoritmosDeOrdenamiento/quicksort.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+
+class Quicksort(object):
+
+	def __init__(self,arreglo):
+		self.arreglo = arreglo
+
+	def ordena(self):
+		last = (len(self.arreglo)-1) 
+		self.quicksort(self.arreglo, 0, last )
+
+	def quicksort(self,arreglo,first,last):
+		if (first >= last): 
+			return
+
+		pivoteIndex = (first + last) / 2
+		pivote = arreglo[pivoteIndex]
+		print "first = %s   last = %s" %(first,last)
+		print "pivoteIndex = %s  valor pivote = %s" %(pivoteIndex, pivote)
+		print "arreglo inicio = %s" %(arreglo)
+		i = first
+		j = last
+
+		while i<j :
+
+			while arreglo[i] < pivote:
+				i += 1
+
+			while arreglo[j] > pivote:
+				j -= 1
+
+			if i < j :
+				print "intercambiando %s con %s" % (i,j)
+				x = arreglo[i]
+				arreglo[i] = arreglo[j]
+				arreglo[j] = x
+			else :
+				break
+			print "arreglo        = %s" %(arreglo)
+
+		print "arreglo fin    = %s" %(arreglo)
+		self.quicksort(arreglo, first, j-1)
+		self.quicksort(arreglo, i+1, last)
+
+
+
+b = Quicksort([2,7,50,3,93,1,4,33,5])
+b.ordena()
+print "Final = %s" %b.arreglo
