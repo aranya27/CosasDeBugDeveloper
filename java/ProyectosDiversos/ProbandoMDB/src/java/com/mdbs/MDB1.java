8c666e8873cc551e854b977efc68b7c06cf771db
armando <armando@COMPUBUG>
2014-09-28 19:22:49 -0500

    Se hace un proyecto en donde pongo ejemplos de MDB

---
+++ java/ProyectosDiversos/ProbandoMDB/src/java/com/mdbs/MDB1.java
@@ -0,0 +1,38 @@
+/*
+Tratar de basarme en estos ejemplos
+http://javahowto.blogspot.mx/2010/04/message-driven-bean-example-with.html
+http://www.roseindia.net/ejb/example-of-messageBean.shtml
+
+
+*/
+
+package com.mdbs;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+
+@MessageDriven(activationConfig = {
+    @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "Cola"),
+    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue")
+})
+public class MDB1 implements MessageListener {
+    
+    public MDB1() {
+    }
+    
+    @Override
+    public void onMessage(Message message) {
+        try{
+            TextMessage tm = (TextMessage)message;
+            System.out.println("MENSAJEEE = "+tm.getText()+". Nombre = "+tm.getStringProperty("nombre"));
+        }catch(JMSException e){
+            e.printStackTrace();
+        }
+    }
+    
+}
