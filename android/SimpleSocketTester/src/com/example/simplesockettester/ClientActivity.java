fa82ffde6fe20929f513a16ff0ac171170d7565e
sabio <thesalad123>
2014-10-19 20:44:46 -0500

    Avance en el socket tester de android

--- android/SimpleSocketTester/src/com/example/simplesockettester/ClientActivity.java
+++ android/SimpleSocketTester/src/com/example/simplesockettester/ClientActivity.java
@@ -1,34 +1,96 @@
 package com.example.simplesockettester;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ClientActivity extends ActionBarActivity {
+	Socket socket;
+	EditText txt_server_address,txt_server_port;
+	TextView txt_info;
+	Button btn_connect;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        
 		setContentView(R.layout.activity_client);
+		btn_connect = (Button)this.findViewById(R.id.btn_connect);
+		txt_server_address = (EditText)this.findViewById(R.id.txt_server_address);
+		txt_server_port = (EditText)this.findViewById(R.id.txt_server_port);
+		txt_info = (TextView)this.findViewById(R.id.txt_info);
+		
+		btn_connect.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				txt_info.setText("");
+				if(socket!=null && socket.isConnected()){
+					try {
+						socket.close();
+					} catch (IOException e) {}
+					finally{
+						socket = null;
+					}
+					setMessageInfo(R.string.socket_not_connected, R.style.txt_info);
+					changeConnectButtonText(R.string.connect);
+				}
+				else{
+					String server = txt_server_address.getText().toString().trim();
+					String port = txt_server_port.getText().toString().trim();
+					
+					if( !server.equals("") && !port.equals("") ){
+						try {
+							socket = new Socket();
+							socket.connect(new InetSocketAddress(server, Integer.parseInt(port)),3000); 
+							setMessageInfo(R.string.socket_connected, R.style.txt_success);
+							changeConnectButtonText(R.string.disconnect);
+						}catch (UnknownHostException e) {
+							setMessageInfo(R.string.unknown_host, R.style.txt_error);
+							e.printStackTrace();
+						} catch (IOException e) {
+							setMessageInfo(R.string.couldn_connect_to_host, R.style.txt_error);
+							e.printStackTrace();
+						}
+						
+					}
+				}
+			}
+		});
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.client, menu);
-		return true;
+	
+	public void setMessageInfo(int idMessage, int idStyle){
+		txt_info.setText(idMessage);
+		txt_info.setTextAppearance(this, idStyle);
 	}
+	
+	public void changeConnectButtonText(int idMessage){
+		btn_connect.setText(idMessage);
+	}
+	
+	
+	
+	
+	class XXX implements Runnable{
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		@Override
+		public void run(){
+			
 		}
-		return super.onOptionsItemSelected(item);
+		
 	}
 }
--- android/SimpleSocketTester/src/com/sockettester/ClientSocketFacade.java
+++ android/SimpleSocketTester/src/com/sockettester/ClientSocketFacade.java
@@ -1,5 +1,17 @@
 package com.sockettester;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class ClientSocketFacade {
+	Socket socket;
+	
+	public void connect(String server, int port) throws IOException{
+		socket = new Socket(server, port);
+	}
+	
+	
+	
+	
 	
 }
