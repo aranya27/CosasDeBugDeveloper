428bb2deb5592096778810c4e590c35259d01972
sabio <thesalad123>
2014-10-21 20:56:16 -0500

    Avance en mi app de android de sockets, ya pude hacer que mande y reciba info como cliente

--- android/SimpleSocketTester/src/com/example/simplesockettester/ClientActivity.java
+++ android/SimpleSocketTester/src/com/example/simplesockettester/ClientActivity.java
@@ -1,26 +1,37 @@
 package com.example.simplesockettester;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Arrays;
+
+import com.sockettester.utils.Util;
 
 import android.support.v7.app.ActionBarActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class ClientActivity extends ActionBarActivity {
 	Socket socket;
-	EditText txt_server_address,txt_server_port;
+	EditText txt_server_address,txt_server_port,txt_input_client,txt_output_client;
 	TextView txt_info;
-	Button btn_connect;
+	Button btn_connect, btn_send, btn_clear_info_to_send, btn_clear_info_from_server;
+	CheckBox chk_hex_client, chk_hex_server;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +45,14 @@ public class ClientActivity extends ActionBarActivity {
 		txt_server_address = (EditText)this.findViewById(R.id.txt_server_address);
 		txt_server_port = (EditText)this.findViewById(R.id.txt_server_port);
 		txt_info = (TextView)this.findViewById(R.id.txt_info);
+		txt_input_client = (EditText)this.findViewById(R.id.txt_input_client);
+		txt_output_client = (EditText)this.findViewById(R.id.txt_output_client);
+		btn_clear_info_to_send = (Button)this.findViewById(R.id.btn_clear_info_to_send);
+		btn_clear_info_from_server = (Button)this.findViewById(R.id.btn_clear_info_from_server);
+		chk_hex_client = (CheckBox)this.findViewById(R.id.chk_hex_client);
+		chk_hex_server = (CheckBox)this.findViewById(R.id.chk_hex_server);
+		btn_send = (Button)this.findViewById(R.id.btn_send);
+		
 		
 		btn_connect.setOnClickListener(new OnClickListener(){
 			@Override
@@ -56,7 +75,10 @@ public class ClientActivity extends ActionBarActivity {
 					if( !server.equals("") && !port.equals("") ){
 						try {
 							socket = new Socket();
-							socket.connect(new InetSocketAddress(server, Integer.parseInt(port)),3000); 
+							socket.connect(new InetSocketAddress(server, Integer.parseInt(port)),2000); 
+							ServerDataHandlerThread sd = new ServerDataHandlerThread();
+							sd.execute();
+							
 							setMessageInfo(R.string.socket_connected, R.style.txt_success);
 							changeConnectButtonText(R.string.disconnect);
 						}catch (UnknownHostException e) {
@@ -71,26 +93,144 @@ public class ClientActivity extends ActionBarActivity {
 				}
 			}
 		});
+		
+		btn_send.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				String infoToSend = txt_input_client.getText().toString();
+				if( socket!=null && socket.isConnected()){
+					byte[] bytesToSend = null;
+					try{
+						if( chk_hex_client.isChecked() ){
+							bytesToSend = Util.fromHexStringToByteArray(infoToSend);
+						}
+						else{
+							bytesToSend = infoToSend.getBytes(Util.CHARSET);
+						}
+					}catch(Exception e){
+						setMessageInfo(R.string.invalid_message, R.style.txt_error);
+					}
+					
+					if(bytesToSend != null ){
+						try{
+							socket.getOutputStream().write(bytesToSend);
+						}catch( Exception e){
+							
+						}
+					}
+				}
+			}
+			
+		});
+		
+		btn_clear_info_to_send.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				txt_input_client.setText("");
+			}
+		});
+		
+		btn_clear_info_from_server.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				txt_output_client.setText("");
+			}
+		});
+		
+		chk_hex_client.setOnCheckedChangeListener(new OnCheckedChangeListener(){
+
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+				Util.changeInfoToBytesStringOrToString(txt_input_client, isChecked);
+			}
+			
+		});
+		
+		chk_hex_server.setOnCheckedChangeListener(new OnCheckedChangeListener(){
+
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+				Util.changeInfoToBytesStringOrToString(txt_output_client, isChecked);
+			}
+			
+		});
+		
 	}
 	
-	public void setMessageInfo(int idMessage, int idStyle){
+	private void setMessageInfo(int idMessage, int idStyle){
 		txt_info.setText(idMessage);
 		txt_info.setTextAppearance(this, idStyle);
 	}
 	
-	public void changeConnectButtonText(int idMessage){
+	private void changeConnectButtonText(int idMessage){
 		btn_connect.setText(idMessage);
+	}
+	
+	private void appendServerAnswer(byte[] answer){
+		StringBuilder result = new StringBuilder( txt_output_client.getText().toString() );
+		if(chk_hex_server.isChecked()){
+			result.append( Util.fromByteArrayToHexString(answer) );
+		}
+		else{
+			try{
+				result.append(new String(answer, Util.CHARSET) );
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		txt_output_client.setText( result.toString() );
+		
 	}
 	
 	
 	
 	
-	class XXX implements Runnable{
-
+	private class ServerDataHandlerThread extends AsyncTask <Void, byte[], Void> {
+		InputStream in = null;
+		
 		@Override
-		public void run(){
-			
+	    protected void onPreExecute() {
+			try {
+				in = socket.getInputStream();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
+		
+		@Override
+	    protected void onProgressUpdate(byte[]... bytes) {
+			appendServerAnswer(bytes[0]);
+		}
+		
+		//appendServerAnswer(content);
+		
+		@Override
+		protected Void doInBackground(Void... params) {
+			byte[] content = new byte[ 2048 ];  
+			
+			if(in != null){
+				int bytesRead;  
+				try {
+					while( ( bytesRead = in.read( content ) ) != -1 ) {  
+						publishProgress(Arrays.copyOfRange(content, 0, bytesRead));
+						//http://stackoverflow.com/questions/10475898/receive-byte-using-bytearrayinputstream-from-a-socket
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} // while  
+			}
+			
+			System.out.println("SE acaba el hilo");
+			return null;
+		}
+		
 		
 	}
 }
---
+++ android/SimpleSocketTester/src/com/sockettester/utils/Util.java
@@ -0,0 +1,74 @@
+package com.sockettester.utils;
+
+import android.widget.EditText;
+
+
+public class Util {
+	public static String CHARSET = "iso-8859-1";
+	
+	
+    //http://javarevisited.blogspot.sg/2013/03/convert-and-print-byte-array-to-hex-string-java-example-tutorial.html
+    public static byte[] fromHexStringToByteArray(String s) throws Exception{
+        s = s.trim();
+        if( (s.length() % 2) != 0) throw new Exception(); 
+        if( s.isEmpty() ) return new byte[0];
+        byte[] array = new byte[s.length()/2];
+        for(int i=0, j=0; j<s.length(); i++, j=j+2){
+            array[i] = (byte)Integer.parseInt(s.substring(j, j+2), 16);
+        }
+        return array;
+    }
+    
+    public static String fromByteArrayToHexString(byte[] array){
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<array.length; i++){
+            sb.append( String.format("%02X", array[i]) );
+        }
+        return sb.toString().toUpperCase();
+    }
+    
+    public static void changeInfoToBytesStringOrToString(EditText editText, boolean toHex){
+    	String infoEditText = editText.getText().toString().trim();
+    	
+    	if( !infoEditText.equals("") ){
+    		try{
+	    		if(toHex){
+	    			editText.setText( fromByteArrayToHexString( infoEditText.getBytes(CHARSET) ) );
+	    		}
+	    		else{
+	    			editText.setText( new String(fromHexStringToByteArray( infoEditText ), CHARSET) );
+	    		}
+    		}catch(Exception e){
+    			editText.setText("");
+    		}
+    	}
+    }
+    
+    public static void main(String[] args) throws Exception{
+        
+        byte[] res = fromHexStringToByteArray("0b0ccc093301");
+        for(int i=0; i<res.length; i++){
+            System.out.print("  "+res[i]);
+        }
+        System.out.println();
+        
+        System.out.print(fromByteArrayToHexString(res));
+        
+        
+        System.out.println();
+        System.out.print("------------------------------");
+        System.out.println();
+        
+        String s = "holáaa";
+        byte[] array = s.getBytes("UTF-8");
+        for(int i=0; i<array.length; i++){
+            System.out.print("  "+array[i]);
+        }
+        System.out.println();
+        System.out.print(new String(array,"UTF-8"));
+        //iso-8859-1
+        
+        System.out.println();
+    }
+    
+}
