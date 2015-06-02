f94ea375607571ca4eccf2b4bea91b200f4db1ae
ec2-user <ec2-user@ip-172-31-21-98.us-west-2.compute.internal>
2015-06-02 17:01:09 -0400

    Se hace el fizzbuzz en python

--- python/cosasDesorganizadas/ejercicios/fizzbuzz/fizzbuzz.py
+++ python/cosasDesorganizadas/ejercicios/fizzbuzz/fizzbuzz.py
@@ -1,2 +1,11 @@
-if __name__ == "__main__":
-	print "hola mundo"
+def fizzbuzz(n):
+	resultado = ""
+	resultado = "Fizz" if n%3==0 else ""
+	resultado = resultado+"Buzz" if n%5==0 else resultado
+	resultado = n if resultado=="" else resultado
+	return resultado
+	
+
+for i in range(1,100):
+	print "%s" % fizzbuzz(i)
+	
