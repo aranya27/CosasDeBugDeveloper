030449b07f60f11ddfbba37243b21f9f7e376713
armando <armando@localhost>
2016-07-11 22:46:26 -0500

    Proyecto para jugar con Spring Security

---
+++ java/ProyectosDiversos/ProbandoSpringSecurity/src/main/java/com/jugando/probandospringsecurity/configuration/SpringMvcInitializer.java
@@ -0,0 +1,23 @@
+package com.jugando.probandospringsecurity.configuration;
+
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+ 
+public class SpringMvcInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+ 
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { HelloWorldConfiguration.class };
+    }
+  
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+  
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+ 
+}
